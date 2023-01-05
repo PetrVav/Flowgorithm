363f50b9f576c92e55372cd172d013023f621da3
Petr <petrv2011@hotmail.com>
2023-01-05 20:09:06 +0300

    first commit

---
+++ Task1_massiv/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] elements = new int[10];
+int sum = 0;
+int index = 0;
+int length = elements.Length;
+
+while (index < length)
+{
+    int current = Random.Shared.Next(30);
+    elements[index] = current;
+    Console.WriteLine(elements[index]);
+
+    if (current >= 10 && current <= 99)
+    {
+        sum = sum + current;
+    }
+    index = index + 1;
+}
+
+Console.WriteLine("---");
+Console.WriteLine(sum);
---
+++ Task2_method/Program.cs
@@ -0,0 +1,59 @@
+﻿int[] CreateArray(int count)
+{
+    return new int[count];
+}
+
+void Fill(int[] elements)
+{
+    int length = elements.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        int current = Random.Shared.Next(30);
+        elements[index] = current;
+        index = index + 1;
+    }
+}
+
+int GetSum(int[] array)
+{
+    int size = array.Length;
+    int sum = 0;
+
+    int position = 0;
+
+    while (position < size)
+    {
+        int current = array[position];
+        if (current >= 10 && current <= 99)
+        {
+            sum = sum + current;
+        }
+        position = position + 1;
+    }
+    return sum;
+}
+
+string Print(int[] collection)
+{
+    string output = String.Empty;
+
+    int size = collection.Length;
+    int index = 0;
+    while (index < size)
+    {
+        int current = collection[index];
+        output = output + $"{current} ";
+        index = index + 1;
+    }
+
+    return output;
+}
+
+int[] arr = CreateArray(10);
+Fill(arr);
+int sum = GetSum(arr);
+Console.WriteLine(Print(arr));
+Console.WriteLine($"sum = {sum}");
+
